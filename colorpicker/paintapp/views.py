85ef4e537214c4ba218b2b9fa89721dd35b82238
storrer <storrer.chris@gmail.com>
2022-03-29 16:42:27 -0700

    Add paintapp/forms.py and ColorPickerView to views.

--- colorpicker/paintapp/views.py
+++ colorpicker/paintapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import View
+from paintapp.forms import ColorPickerForm
+
+class ColorPickerView(View):
+    def get(self, request):
+        """Present the color picker form and color the user"""
+        form = ColorPickerForm()
+
+        context = {
+            'form': form,
+            'red': 255,
+            'green': 255,
+            'blue': 255,
+        }
+
+        return render(request, 'color_picker.html', context=context)
